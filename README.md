d54befe73b269425f950985fb1ce57d56bbc1922
Martin Clason <martin@clason.se>
2019-12-25 14:31:07 +0100

    It works! You can press a button to display code and connect to the wifi and the webpage is served.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## How to compile and flash board
 Execute this command in the project directory:
 ```
-make -f makeEspArduino.mk UPLOAD_PORT=/dev/tty.usbserial-14330 flash
+make -f makeEspArduino.mk UPLOAD_PORT=/dev/tty.usbserial-14330 SKETCH=sketch.ino flash
 ```
 * Note that all included libraries must be placed in the project directory.
 * You may also need to specify the port of the device. Do that by setting the `UPLOAD_PORT` variable when running make.
--- sketch.ino
+++ sketch.ino
@@ -4,48 +4,158 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <LOLIN_I2C_BUTTON.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+/* Adafruit_SSD1306 display(0); */
+
+#if (SSD1306_LCDHEIGHT != 48)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+#define QR_CODE_HEIGHT 25
+#define QR_CODE_WIDTH  25
+#define PADDING 5
+
+static const unsigned char PROGMEM qr_code_bmp[] =
+{
+    B11111110, B01110100, B10111111, B10000000,
+    B10000010, B00011000, B00100000, B10000000,
+    B10111010, B01110010, B10101110, B10000000,
+    B10111010, B10110011, B10101110, B10000000,
+    B10111010, B10101100, B00101110, B10000000,
+    B10000010, B01100110, B00100000, B10000000,
+    B11111110, B10101010, B10111111, B10000000,
+    B00000000, B01001100, B10000000, B00000000,
+    B11000111, B01101011, B10001100, B00000000,
+    B00100001, B10100011, B11001111, B00000000,
+    B01000110, B11011001, B00110101, B10000000,
+    B01101000, B10110011, B10001100, B10000000,
+    B11100110, B01100110, B11100000, B10000000,
+    B10100000, B00101111, B00000001, B00000000,
+    B10011110, B01100111, B01010101, B10000000,
+    B10011101, B10001110, B10001010, B10000000,
+    B10101111, B00101010, B11111010, B00000000,
+    B00000000, B11100001, B10001010, B00000000,
+    B11111110, B11101110, B10101100, B10000000,
+    B10000010, B11110010, B10001000, B00000000,
+    B10111010, B01010001, B11111110, B10000000,
+    B10111010, B00101100, B00110101, B10000000,
+    B10111010, B00100110, B01000010, B10000000,
+    B10000010, B10101101, B10111000, B10000000,
+    B11111110, B10011101, B10100100, B10000000,
+};
 
 Adafruit_SSD1306 display{-1};//SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1};
-
-static const uint8_t qr_image_data[16] {
-    0xff, 0x00, 0x00, 0xff,
-    0x00, 0xff, 0xff, 0x00,
-    0x00, 0xff, 0xff, 0x00,
-    0xff, 0x00, 0x00, 0xff
-    };
-
+I2C_BUTTON button(DEFAULT_I2C_BUTTON_ADDRESS); //I2C Address 0x31
 ESP8266WebServer server{80};
 
+bool is_displaying {false};
+long start_displaying_time;
+const long display_time {15*1000};
+const long poll_button_interval {20};
+long last_poll_time;
+long current_time;
 String header;
+
+void displayQRCode(uint8_t x, uint8_t y)
+{
+    display.drawBitmap(x, y, qr_code_bmp, QR_CODE_WIDTH, QR_CODE_HEIGHT, BLACK);
+}
+
+void displayWiFiName(uint8_t x, uint8_t y)
+{
+    display.setTextColor(BLACK);
+    display.setTextSize(1);
+    display.setCursor(PADDING + QR_CODE_WIDTH + PADDING - 1, PADDING + QR_CODE_HEIGHT - 6);
+    display.print("WiFi:");
+    display.setCursor(PADDING + QR_CODE_WIDTH - 6, PADDING + QR_CODE_HEIGHT + PADDING);
+    display.print("JulIoT");
+}
+
+void displayInfo(long t)
+{
+    display.clearDisplay();
+
+    display.fillScreen(WHITE);
+    //displayQRCode(8, (SSD1306_LCDHEIGHT - QR_CODE_HEIGHT) / 2);
+    displayQRCode(PADDING, PADDING);
+    displayWiFiName(5, QR_CODE_HEIGHT + 5);
+    //display.drawBitmap(8,8,qr_code_bmp, QR_CODE_WIDTH, QR_CODE_HEIGHT, BLACK);
+
+    display.display();
+    start_displaying_time = t;
+    is_displaying = true;
+}
+
+void clearDisplay()
+{
+    display.clearDisplay();
+    display.display();
+    is_displaying = false;
+}
+
+bool shouldDisplay(long t)
+{
+    return t < start_displaying_time + display_time;
+}
 
 void handleRoot()
 {
-    server.send(200, "text/html", "<h1>God Jul</h1>");
+    server.send(200, "text/html", "<h1>God jul Pappa önskar Martin!</h1>");
 }
 
 void setup()
 {
+    // init serial port for debugging
     Serial.begin(115200);
 
+    // config server
     bool result {WiFi.softAP("JulIoT")};
     if (!result)
     {
         Serial.println("Failed to setup WiFi");
     }
 
-    // config server
     server.on("/", handleRoot);
-    
+
     Serial.print("IP: ");
     Serial.println(WiFi.softAPIP());
-    
+
     server.begin();
+
+    // init time
+    last_poll_time = millis();
+
+    // init display
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)    
+    clearDisplay();
 }
 
 void loop()
 {
-  server.handleClient();
+    server.handleClient();
+    current_time = millis();
+    if (is_displaying)
+    {
+        if (!shouldDisplay(current_time))
+        {
+            clearDisplay();
+        }
+    }
+
+    if (current_time > last_poll_time + poll_button_interval)
+    {
+        if (button.get() == 0) // button press
+        {
+            if (button.BUTTON_A)
+            {
+                displayInfo(current_time);
+            }
+            
+            if (button.BUTTON_B)
+            {
+                displayInfo(current_time);
+            }
+        }
+    }
 }
